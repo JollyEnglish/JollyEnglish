537f5e5676c63308ff2a0eae74ed13150459862a
Jakub Korsak <korsakuba@gmail.com>
2019-03-31 12:45:13 +0200

    removed _site

--- _site/assets/js/scroll.js
+++
@@ -1,9 +0,0 @@
-function scroller(id){
-    if (id=="home"){
-        document.body.scrollTop = 0;
-    }
-    document.querySelector('#'+id).scrollIntoView({
-        behavior: 'smooth'
-    });
-}
-
